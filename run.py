ce1d3ff1c852bb41d40a6052e63a07dea1fe56c4
SaraabbasiNZ <>
2024-01-07 13:38:37 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+class Board:
+    def __init__(self):
+        # Initialize the game board with default settings and parameters
+        self.board_size = 5
+        self.ship_size = 3
+        self.player_board = [[' ' for _ in range(self.board_size)] for _ in range(self.board_size)]
+        self.computer_board = [[' ' for _ in range(self.board_size)] for _ in range(self.board_size)]
+        self.player_turns = 20
+        self.computer_turns = 20
+        self.ships = 3
